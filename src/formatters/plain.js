6ef53f77f450de596f7e929bd34f8d724c5baeba
runabal <nvbalandina@mail.ru>
2023-02-12 14:18:12 +0300

    update stylysh and plain

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,28 +1,35 @@
 import _ from 'lodash';
 
-const stringify = (nodeValue) => {
-  if (_.isObject(nodeValue)) {
+const stringify = (data) => {
+  if (_.isObject(data)) {
     return '[complex value]';
   }
-  return typeof nodeValue === 'string' ? `'${nodeValue}'` : nodeValue;
-};
-
-const getFormatPlain = (node, parent = '') => {
-  switch (node.type) {
-    case 'added':
-      return `Property '${parent}${node.key}' was added with value: ${stringify(node.value)}`;
-    case 'deleted':
-      return `Property '${parent}${node.key}' was removed`;
-    case 'unchanged':
-      return null;
-    case 'changed':
-      return `Property '${parent}${node.key}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
-    case 'nested':
-      return node.children.map((value) => getFormatPlain(value, `${parent + node.key}.`))
-        .filter((item) => item !== null).join('\n');
-    default:
-      throw new Error(`Unknown type of data ${node.tpe}`);
+  if (typeof data === 'string') {
+    return `'${data}'`;
   }
+  return String(data);
 };
 
-export default (formatPlain) => `${formatPlain.map((tree) => getFormatPlain(tree)).join('\n')}`.trim();
+const iter = (children, parent) => {
+  const lines = children.map((node) => {
+    switch (node.type) {
+      case 'added':
+        return `Property '${parent}${node.key}' was added with value: ${stringify(node.value)}`;
+      case 'deleted':
+        return `Property '${parent}${node.key}' was removed`;
+      case 'unchanged':
+        return null;
+      case 'changed':
+        return `Property '${parent}${node.key}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
+      case 'nested':
+        return iter(node.children, `${parent}${node.key}.`);
+      default:
+        throw new Error(`Unknown type of data ${node.tpe}`);
+    }
+  });
+  return lines.filter((item) => item !== null).join('\n');
+};
+
+const formatPlain = (tree) => iter(tree, '');
+
+export default formatPlain;
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,18 +2,20 @@ import _ from 'lodash';
 
 const makeIndent = (depth, spaces = 4) => ' '.repeat(depth * spaces - 2);
 
-const stringify = (nodeValue, depth = 1) => {
-  if (!_.isObject(nodeValue)) {
-    return nodeValue;
+const stringify = (data, depth) => {
+  if (!_.isObject(data)) {
+    return String(data);
   }
-  const keys = Object.keys(nodeValue);
-  const getKeys = keys.map(
-    (key) => ` ${makeIndent(depth + 1)} ${key}: ${stringify(nodeValue[key], depth + 1)}`,
+  const keys = Object.keys(data);
+  const lines = keys.map(
+    (line) => `${makeIndent(depth + 1)}  ${line}: ${stringify(data[line], depth + 1)}`,
   );
-  return `{\n${getKeys.join('\n')}\n ${makeIndent(depth)} }`;
+  const output1 = `{\n${lines.join('\n')}`;
+  const output2 = `${makeIndent(depth)}`;
+  return `${output1}\n  ${output2}}`;
 };
 
-const getFormatStylish = (node, depth = 1) => {
+const iter = (children, depth) => children.map((node) => {
   switch (node.type) {
     case 'added':
       return `${makeIndent(depth)}+ ${node.key}: ${stringify(node.value, depth)}`;
@@ -21,13 +23,20 @@ const getFormatStylish = (node, depth = 1) => {
       return `${makeIndent(depth)}- ${node.key}: ${stringify(node.value, depth)}`;
     case 'unchanged':
       return `${makeIndent(depth)}  ${node.key}: ${stringify(node.value, depth)}`;
-    case 'changed':
-      return `${makeIndent(depth)}- ${node.key}: ${stringify(node.value1, depth)}\n${makeIndent(depth)}+ ${node.key}: ${stringify(node.value2, depth)}`;
-    case 'nested':
-      return `${makeIndent(depth)}  ${node.key}: {\n${node.children.map((a) => getFormatStylish(a, depth + 1)).join('\n')}\n ${makeIndent(depth)} }`;
+    case 'changed': {
+      const line1 = `${makeIndent(depth)}- ${node.key}: ${stringify(node.value1, depth)}`;
+      const line2 = `${makeIndent(depth)}+ ${node.key}: ${stringify(node.value2, depth)}`;
+      return `${line1}\n${line2}`;
+    }
+    case 'nested': {
+      const line3 = iter(node.children, depth + 1);
+      return `${makeIndent(depth)}  ${node.key}: {\n${line3.join('\n')}\n ${makeIndent(depth)} }`;
+    }
     default:
       throw new Error(`Unknown type of data ${node.type}!`);
   }
-};
+});
 
-export default (formatStylish) => `{\n${formatStylish.map((tree) => getFormatStylish(tree, 1)).join('\n')}\n}`;
+const formatStylish = (tree) => `{\n${iter(tree, 1).join('\n')}\n}`;
+
+export default formatStylish;
